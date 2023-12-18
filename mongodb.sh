fd0bdac235fa40c1a56dc70c6a54e542764879e0
RajanikanthKore <rajanikanthkore2@gmail.com>
2023-12-18 14:02:10 +0530

    roboshop mongodb shell code updated

---
+++ mongodb.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%S-%M-%H)
+LOGFILE="/tmp/$0-TIMESTAMP.log"
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+  then 
+      echo -e "ERROR:$R Root user access denied $N"
+else
+      echo -e "You are the $Y root user $N"
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+      then 
+          echo "ERROR: $2 ...failed!!"
+    else
+          echo "$2 ....successed!!"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+cp mongo.repo /etc/yum.repos.d/
+
+dnf install mongodb-org -y &>>LOGFILE
+
+VALIDATE $? "Copyied!!"
